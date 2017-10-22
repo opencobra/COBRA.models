3d61d290a4260737d699a38491e1896d426e70c9
laurentheirendt <laurent.heirendt@uni.lu>
2017-10-22 14:33:07 +0200

    Comparison between SHA

--- updateModels.jl
+++ updateModels.jl
@@ -1,10 +1,30 @@
 using JSON
 using Requests
 
+# create a temporary directory
+tempDirPath = mktempdir()
+
+# parse the JSON list of models
 listModels = JSON.parsefile("models.json")
 
+# loop through the models and download them
 for model in listModels
-    println(model["name"])
+    # download the file name
     modelFile = get(model["url"])
-    save(modelFile, model["name"])
+    modelName = tempDirPath*"/"*model["name"]
+    modelExt = model["name"][end-3:end]
+    save(modelFile, modelName)
+
+    # get the original model
+    modelOrig = modelExt[2:end]*"/"*model["name"]
+
+    # save the checksum
+    checkSum = Base.crc32c(read(modelName))
+    checkSumOrig = Base.crc32c(read(modelOrig))
+
+    if checkSum == checkSumOrig
+        println("SAME: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+    else
+        println("DIFFERENT: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+    end
 end
