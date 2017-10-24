78e297a60d4f9ff76458ba6795846100e24e5c40
laurentheirendt <laurent.heirendt@uni.lu>
2017-10-24 15:31:41 +0200

    Sending email messages

--- updateModels.jl
+++ updateModels.jl
@@ -7,6 +7,8 @@ tempDirPath = mktempdir()
 # parse the JSON list of models
 listModels = JSON.parsefile("models.json")
 
+println(" > The temporary directory is: $tempDirPath")
+
 # loop through the models and download them
 for model in listModels
     # download the file name
@@ -23,8 +25,10 @@ for model in listModels
     checkSumOrig = Base.crc32c(read(modelOrig))
 
     if checkSum == checkSumOrig
-        println("SAME: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+        println(" - SAME: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+        run(pipeline(`echo "[COBRA.models] Models are the same"`, `mail -s "- [COBRA.models] Models are the same" laurent.heirendt@uni.lu`))
     else
-        println("DIFFERENT: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+        println(" + DIFFERENT: $(model["name"]) [new: $checkSum | orig: $checkSumOrig]")
+        run(pipeline(`echo "[COBRA.models] Models are different"`, `mail -s "+ [COBRA.models] Models are different" laurent.heirendt@uni.lu`))
     end
 end
